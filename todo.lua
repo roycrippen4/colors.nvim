b65ac1207fabf3c186e6bf935d9d667a0112cc0e
roy.crippen4 <roy.crippen4@archarithms.com>
2024-02-18 00:38:44 -0500

    (chore) update todo

--- todo.lua
+++ todo.lua
@@ -1,18 +1,17 @@
--- BUG: Fix replacing a color in a different format.
--- Currently getting an `invalid winnr` error.
--- TODO: Confirm the above bug is still happening.
--- I haven't had any errors like that recently.
+--- TODO: Better tailwind detection.
+---   We may need to abandon using tailwind colors alltogether.
+---   Detection is very dificult due to the highly composable nature.
+---   If I can figure out a way to differentiate between 'slate-50' and 'slate-500' we can leave the support as it stands.
 
 -- TODO: Update the readme.
 -- It is so inaccurate and misleading right now.
 -- Needs images of the UI stuff (I'm pretty proud of it so show it off)
 -- Better explanations for the features offered and how to use them.
---
 
 -- TODO: Overhaul current css implementation
 --   LOW PRIORITY:  Should also be able to take the current color and match it with the closest current css color name.
 --   HIGH PRIORITY: Should still build a native UI for those who don't use telescope
---   HIGH PRIORITY: If replacing with css value then it should be in any format offered by other tools
+--   DONE: HIGH PRIORITY: If replacing with css value then it should be in any format offered by other tools
 --   DONE: HIGH PRIORITY: Fix the telescope user configuration. Currently it does not work and uses the default
 --   DONE: HIGH PRIORITY: Ensure telescope respects the configuration settings with the selection stuff
 --   DONE: Should support popular UI-library color names
@@ -23,6 +22,10 @@
 -- BUG: DONE: Fix picker help win
 -- BUG: DONE: Fix picker export
 -- BUG: DONE: Fix bar colors
--- TODO:  DONE: Replace if color under cursor by default.
--- TODO:  DONE: Finish fixing up the help window
--- TODO:  DONE: Select a color from css table - Built the telescope extension for this.
+-- TODO: DONE: Replace if color under cursor by default.
+-- TODO: DONE: Finish fixing up the help window
+-- TODO: DONE: Select a color from css table - Built the telescope extension for this.
+-- BUG: DONE: Fix replacing a color in a different format.
+--       Currently getting an `invalid winnr` error.
+-- TODO: DONE: Decide whether or not to use 3-digit hex
+--       We will not be supporting 3-digit hex
